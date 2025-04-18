e96b0cd70e33f7f4faca906082d2ed4626769825
sahal <sahalchalukolappa@gmail.com>
2025-04-18 21:27:53 +0530

    Improved now functioning

--- client.py
+++ client.py
@@ -1,39 +1,95 @@
-import socket
+
+import socket 
 import threading
 
 
-HOST = socket.gethostbyname(socket.gethostname())
-PORT = 5050
+class Debug:
+    def __init__(self):
+        self.Error = "\033[31m"   # Red text
+        self.Success = "\033[32m" # Green text
+        self.Warn = "\033[33m"    # Yellow text
+        self.end = "\033[0m"      # Reset color
 
-def log(msg, sender='CLIENT'):
-    print(f"[{sender}] {msg}")
+    def log(self, message="", sender="", state='success'):
+        color_table = {
+            "success": f"{self.Success}[{sender}] {message}{self.end}",
+            "error": f"{self.Error}[{sender}] {message}{self.end}",
+            "warn": f"{self.Warn}[{sender}] {message}{self.end}",
+        }
 
-def send(connection):
-    while True:
-        user = input("[SERVER]: ")    
-        connection.sendall(user.encode("utf-8"))
-        if user == "disconnect":
-            connection.close()
+        if state in color_table:
+            print(color_table[state])
+        else:
+            print(f"[{sender}] {message}")
 
-def recieve(connection):
-    while True:
-        client = connection.recv(1024)
-        if  client.decode("utf-8") == "disconnect":
-            connection.close()
-        log(client.decode("utf-8"), "CLIENT")
+class Client:
+    def __init__(self, ip, port, sender="CLIENT"):
+        self.port = port
+        self.ip = ip
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.debug = Debug()
+        self.format = "utf-8"
+        self.sender_name = sender
+
+    def init(self):
+        self.debug.log("Connecting to server.", sender=self.sender_name, state="normal")
+        try:
+            self.socket.connect((self.ip, self.port))
+            self.debug.log("Connection succesful.", sender=self.sender_name, state="success")
+        except Exception as e:
+            self.debug.log(f"Connection failed: {e}", sender=self.sender_name, state="error")
+
+    def send(self, message: str):
+        in_bytes = message.encode(self.format)
+        self.socket.sendall(in_bytes)
+
+    def receive(self):
+        message = self.socket.recv(1024)
+        self.debug.log(message=message, sender=self.sender_name, state="")
+        
+
+    def close_connection(self):
+        self.socket.close()
 
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    log("Connecting to server")
-    s.connect((HOST, PORT))
-    log("Connection succesfully established")
+class PeerToPeerMessaging:
+    def __init__(self, ip, port, sender):
+        self.client = Client(ip=ip, port=port, sender=sender)
+        self.client.init()
+        self.end_word = 'disconnect'
+        self.sender_name = sender
+        self.debug = Debug()
+        self.message_threads_running = True
 
-    send_thread = threading.Thread(target=send, args=[s])
-    recieve_thread = threading.Thread(target=recieve, args=[s])
+    def send_loop(self):
+        while self.message_threads_running:
+            user = input(f"[{self.sender_name}]: ")
+            self.client.send(message=user)
+            if self.end_word == user:
+                self.client.close_connection()
+                self.message_threads_running = False
 
-    send_thread.start()
-    recieve_thread.start()
+    def receive_loop(self):
+        while self.message_threads_running:
+            message = self.client.receive()
+            self.debug.log(message=message, sender='SERVER', state="")
+            if message == self.end_word:
+                self.client.close_connection()
+                self.message_threads_running = False
 
-    
+    def initialize_threads(self):
+        self.sender_thread = threading.Thread(target=self.send_loop)
+        self.receiver_thread = threading.Thread(target=self.receive_loop)
+        return True
 
-log("session closed")
+    def start(self):
+        self.initialize_threads()
+        self.receiver_thread.start()
+        self.sender_thread.start()
+
+
+ip = socket.gethostbyname(socket.gethostname())
+port = 5050
+
+ptp_chat = PeerToPeerMessaging(ip, port, "Sahal")
+ptp_chat.start()
--- server.py
+++ server.py
@@ -1,42 +1,97 @@
-import socket
 import threading
-
-HOST = socket.gethostbyname(socket.gethostname())
-PORT = 5050
-
-def log(msg, sender='SERVER'):
-    print(f"[{sender}] {msg}")
-
-def send(connection):
-    user = input("[SERVER]: ")
-    
-    connection.sendall(user.encode("utf-8"))
-    if  user == "disconnect":
-        connection.close()
-
-def recieve(connection):
-    
-    client = connection.recv(1024)
-    if  client.decode("utf-8") == "disconnect":
-            connection.close()
-                
-    log(client.decode("utf-8"), "CLIENT")
+import socket
 
 
+class Debug:
+    def __init__(self):
+        self.Error = "\033[31m"   # Red text
+        self.Success = "\033[32m" # Green text
+        self.Warn = "\033[33m"    # Yellow text
+        self.end = "\033[0m"      # Reset color
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    log("Binding the socket...")
-    s.bind((HOST, PORT))
-    log("Socket binded to host address.")
-    log("Listening...")
-    s.listen()
-    
-    
-    connection, addr = s.accept()
-    send_thread = threading.Thread(target=send, args=[connection])
-    recieve_thread = threading.Thread(target=recieve, args=[connection])
-    
-    send_thread.start()
-    recieve_thread.start()
-    
-log("Session closed.")
+    def log(self, message="", sender="", state='success'):
+        color_table = {
+            "success": f"{self.Success}[{sender}] {message}{self.end}",
+            "error": f"{self.Error}[{sender}] {message}{self.end}",
+            "warn": f"{self.Warn}[{sender}] {message}{self.end}",
+        }
+
+        if state in color_table:
+            print(color_table[state])
+        else:
+            print(f"[{sender}] {message}")
+
+class Server:
+    def __init__(self, ip, port, sender):
+        self.port = port
+        self.ip = ip
+        self.sender_name = sender
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.debug = Debug()
+        self.format = "utf-8"
+
+    def init(self):
+        self.debug.log("Connecting the socket.", sender=self.sender_name, state="")
+        self.socket.bind((self.ip, self.port))
+        self.socket.listen()
+        self.connection, self.client_address = self.socket.accept()
+        self.debug.log("Connection succesful.", sender=self.sender_name, state="success")
+
+    def send(self, message: str):
+        in_bytes = message.encode(self.format)
+        self.connection.sendall(in_bytes)
+
+    def receive(self):
+        message = self.connection.recv(1024).decode(self.format)
+        self.debug.log(message=message, sender=self.sender_name, state="")
+        
+
+    def close_connection(self):
+        self.socket.close()
+
+
+class PeerToPeerMessaging:
+    def __init__(self, ip, port, sender):
+        self.server = Server(ip=ip, port=port, sender=sender)
+        self.server.init()
+        self.end_word = 'disconnect'
+        self.sender_name = sender
+        self.debug = Debug()
+        self.message_threads_running = True
+
+    def send_loop(self):
+        while self.message_threads_running:
+            user = input(f"[{self.sender_name}]: ")
+            self.server.send(message=user)
+            if self.end_word == user:
+                self.server.close_connection()
+                self.message_threads_running = False
+
+    def receive_loop(self):
+        while self.message_threads_running:
+            message = self.server.receive()
+            self.debug.log(message=message, sender='SERVER', state="")
+            if message == self.end_word:
+                self.server.close_connection()
+                self.message_threads_running = False
+
+    def initialize_threads(self):
+        self.sender_thread = threading.Thread(target=self.send_loop)
+        self.receiver_thread = threading.Thread(target=self.receive_loop)
+        return True
+
+    def start(self):
+        self.initialize_threads()
+        self.sender_thread.start()
+        self.receiver_thread.start()
+
+
+ip = socket.gethostbyname(socket.gethostname())
+port = 5050
+
+ptp_chat = PeerToPeerMessaging(ip, port, "Sahal")
+ptp_chat.start()
+
+
+dbg = Debug()
+dbg.log(message="test", sender="Server", state="success")
